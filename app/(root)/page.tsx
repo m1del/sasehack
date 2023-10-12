26e577afaabf9a1ea9b2b6c088527cca16a44e26
John Miclat <jmiclat@ufl.edu>
2023-10-12 00:51:31 -0400

    Add delay to home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 
 export default function Home() {
 
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      window.location.href = '/grocerylist'
+    }, 3000);
+
+    return () => clearTimeout(timerId);
+  }, []);
+    
+  
   return (
     <div className='home-page'>
       <Image
