59f58e9d37a7cb3f12a170f3a97470a0b1b63213
John Miclat <jmiclat@ufl.edu>
2023-10-12 03:45:03 -0400

    Fix image loading for expiring foods

--- app/components/ExpiringFood.js
+++ app/components/ExpiringFood.js
@@ -1,12 +1,15 @@
 "use client";
-import { IconContext } from "react-icons";
-import { IoClose } from "react-icons/io5";
 
 export default function ExpiringFood({ name, expires }) {
+
+  function getImagePath(foodName) {
+    return `/assets/${foodName.toLowerCase()}.png`;
+  }
+
   return (
     <>
       <div className="expire_container">
-        <img className="food_img" src="/assets/banana.jpg" />
+        <img className="food_img" src={getImagePath(name)} />
         <div>
           <h2 className="food_header">{name}</h2>
           <p className="food_expire">{expires} days left!</p>
