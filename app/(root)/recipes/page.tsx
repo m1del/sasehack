fd5fbc095c76319321fff58991e4925618f86692
John Miclat <jmiclat@ufl.edu>
2023-10-12 03:45:58 -0400

    Fix recipe API fetch

--- app/(root)/recipes/page.tsx
+++ app/(root)/recipes/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 import TopBar from "@/app/components/TopBar";
+import TypeAnim from "@/app/components/TypeAnim";
 import { config } from "dotenv";
 import { useEffect, useState } from "react";
 import ExpiringFood from "../../components/ExpiringFood";
-import TypeAnim from "@/app/components/TypeAnim";
 config();
 
 function ExpiringFoodList() {
   const [expire, setExpire] = useState([]);
-  const [recipe, setRecipe] = useState("Pinging OpenAi...");
-  const [repeat, setRepeat] = useState(Infinity);
+  const [recipe, setRecipe] = useState("Generating Recipe . . .");
+  const [loading, setLoading] = useState(true);
+
+
   useEffect(() => {
     async function fetchFoods() {
       try {
@@ -26,6 +28,7 @@ function ExpiringFoodList() {
       }
     }
     fetchFoods();
+    
     async function makeRecipe() {
       try {
         console.log("pinging openAI...");
@@ -37,18 +40,13 @@ function ExpiringFoodList() {
         setRecipe(data.recipe);
       } catch (error) {
         console.error("Failed to update expiration dates:", error);
+      } finally {
+        setLoading(false);
       }
     }
     makeRecipe();
   }, []);
 
-  const prompt =
-    "I have most cookware available at my disposal such as a stove, oven, microwave, air fryer, toaster, kettle, along with utensils like kitchen knives, spatulas, etc. ";
-  ("I have these ingredients: {ingredients}. ");
-  ("Give me a recipe that includes most, if not all of these ingredients. ");
-  ("You may include other ingredients from my kitchen, such as {inventory} ");
-  ("and items that are common to most households, like salt, pepper, oregano, and garlic powder. ");
-  ("What can I make as well as the instructions?");
 
   return (
     <>
@@ -61,7 +59,11 @@ function ExpiringFoodList() {
         </div>
 
         <div className="text-box">
-          <TypeAnim text={recipe} inf={Infinity} />
+          {loading ? (
+            <p>Loading recipe...</p>
+          ) : (
+            <TypeAnim text={recipe} inf={Infinity} />
+          )}
         </div>
       </div>
     </>
